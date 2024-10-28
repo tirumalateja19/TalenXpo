4a7990fe598c9597ca52f7be9329ec6f6679df3e
tirumalateja19 <tirumalateja.jampani@gmail.com>
2024-10-28 18:46:37 +0530

    Almost done

--- AProject.js
+++ AProject.js
@@ -1,25 +1,51 @@
+const header = document.querySelector("[data-header]");
+
+const headerActive = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+};
+
+// Listen for scroll events and trigger the headerActive function
+window.addEventListener("scroll", headerActive);
+
 document.querySelector(".search-btn").addEventListener("click", function () {
   alert("Search feature is currently not implemented.");
 });
 const panels = document.querySelectorAll(".panel");
 
 // optional Changing cards
-  const links = document.querySelectorAll(".optional-sidebar a");
-  const cards = document.querySelectorAll(".optional-card");
+const links = document.querySelectorAll(".optional-sidebar a");
+const cards = document.querySelectorAll(".optional-card");
 
-  links.forEach((link) => {
-    link.addEventListener("click", function (event) {
-      event.preventDefault(); // Prevents the page from redirecting or reloading
+links.forEach((link) => {
+  link.addEventListener("click", function (event) {
+    event.preventDefault(); // Prevents the page from redirecting or reloading
 
-      links.forEach((l) => l.classList.remove("optional-active"));
-      this.classList.add("optional-active");
+    links.forEach((l) => l.classList.remove("optional-active"));
+    this.classList.add("optional-active");
 
-      cards.forEach((card) => card.classList.remove("optional-active"));
+    cards.forEach((card) => card.classList.remove("optional-active"));
 
-      const cardId = this.id + "-card";
-      document.getElementById(cardId).classList.add("optional-active");
-    });
+    const cardId = this.id + "-card";
+    document.getElementById(cardId).classList.add("optional-active");
   });
+});
+
+// steps
+window.addEventListener("load", () => {
+  const steps = document.querySelectorAll(".steps-list-opps li");
+
+  steps.forEach((step, index) => {
+    setTimeout(() => {
+      step.style.opacity = "1";
+      step.style.transform = "translateY(0)";
+    }, 1900 * index);
+  });
+});
+
 
 // Expanding cards
 panels.forEach((panel) => {
